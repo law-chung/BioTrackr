520676a72f168363d60a0daef79b4bcd5574ff2b
law-chung <lawrence.chung.nc@gmail.com>
2024-11-13 23:17:18 -0500

    whitespacing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
     crossOrigin=""></script>
+    
 function App() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
