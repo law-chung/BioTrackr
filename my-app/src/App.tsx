e59bf39e4cee076a810242824a35d31e57c42c8f
law-chung <lawrence.chung.nc@gmail.com>
2024-11-13 23:15:23 -0500

    BioTrackr project initial upload

---
+++ my-app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import './App.css';
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossOrigin=""></script>
+function App() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default App;
