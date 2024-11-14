a4a5e8b793d2b47f72c0ffc23ea83517cef89763
law-chung <lawrence.chung.nc@gmail.com>
2024-11-14 12:15:15 -0500

    tesla charger location mini task

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import './App.css';
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-    crossOrigin=""></script>
+import teslaData from "./data/tesla-sites.json"
     
 function App() {
+
+const filteredStations = teslaData.filter(tsla => tsla.address.country === "Italy") 
+
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[42.5, 13.2]} zoom={7} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+      {filteredStations.map(tsla => (
+        <Marker
+        key = {tsla.id}
+        position={[tsla.gps.latitude, tsla.gps.longitude]}>
+        <Popup position={[tsla.gps.latitude, tsla.gps.longitude]}>
+        <div>
+          <h2>{"Name: " + tsla.name}</h2>
+          <p>{"Stall count: " + tsla.stallCount}</p>
+          <p>{"Status: " + tsla.status}</p>
+        </div>
         </Popup>
-      </Marker>
+
+        </Marker>
+
+      ))}
+
     </MapContainer>
   );
 }
